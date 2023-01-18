0b4f1065f22eb53c2e0d21c7149b5f67aa1a5b25
RuslanaCat <leoniecat.uwu@gmail.com>
2023-01-18 09:57:04 +0100

    Bereinigung

---
+++ LS2_Konfiguration/src/Volumen.java
@@ -0,0 +1,16 @@
+
+public class Volumen {
+
+	public static double volumen_rechnung() {
+		double volumen;
+		
+		
+		return volumen;
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
