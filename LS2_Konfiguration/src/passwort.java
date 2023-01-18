6d472f97b6a0196ab91a6c2318f9b1c94ebe7af5
RuslanaCat <leoniecat.uwu@gmail.com>
2023-01-18 10:16:03 +0100

    passwort aufgabe hinzugefügt

---
+++ LS2_Konfiguration/src/passwort.java
@@ -0,0 +1,25 @@
+
+public class passwort {
+	
+	public static boolean checkPasscode(int H4X0R) {
+	    // gibt true zurück, falls das Passwort korrekt ist.
+	    boolean result = false; for (int E1337 = 42; E1337 <= (52 ^ (0x6c));
+	    E1337 += (3 << (14 % 6))) { if (result = ((++E1337 | E1337 + (2 >>> 1)) ^ (1 + H4X0R)) == (123456789 & 0)) break; } return result;
+	}
+
+	public static void main(String[] args) {
+		boolean ergebniss;
+		int Passwort;
+		int zähler = 0;
+		
+		for (Passwort = 1; Passwort <= 1000; Passwort++) {
+			ergebniss = checkPasscode(Passwort);
+			if (ergebniss) {
+				zähler=zähler+1;
+				System.out.println("Das "+zähler+". Passwort ist: "+Passwort);
+			}
+		}
+
+	}
+
+}
