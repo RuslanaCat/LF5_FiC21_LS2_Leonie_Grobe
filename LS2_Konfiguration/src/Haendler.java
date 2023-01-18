d2216cb0ed440da05710745540bc65c62e76966c
RuslanaCat <leoniecat.uwu@gmail.com>
2023-01-18 10:34:30 +0100

    Haendler datei hinzugefügt

---
+++ LS2_Konfiguration/src/Haendler.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+public class Haendler {
+	
+	public static String liesString(Scanner myScanner) {
+		System.out.println("was möchten Sie bestellen?");
+        String artikel = myScanner.next();
+        return artikel;
+	}
+	
+	public static int liesInt(Scanner myScanner) {
+		System.out.println("Geben Sie die Anzahl ein:");
+        int anzahl = myScanner.nextInt();
+        return anzahl;
+	}
+	
+	public static double liesDoublepreis(Scanner myScanner) {
+		System.out.println("Geben Sie den Nettopreis ein:");
+        double preis = myScanner.nextDouble();
+        return preis;
+	}
+	
+	public static double liesDoublemwst(Scanner myScanner) {
+		System.out.println("Geben Sie den Mehrwertsteuersatz in Prozent ein:");
+        double mwst = myScanner.nextDouble();
+		return mwst;
+	}
+	
+	public static double berechneGesamtnettopreis(int anzahl, double nettopreis) {
+		double nettogesamtpreis = anzahl * nettopreis;
+		return nettogesamtpreis;
+	}
+	
+	public static double berechneGesamtbruttopreis(double nettogesamtpreis, double mwst) {
+		double bruttogesamtpreis = nettogesamtpreis * (1 + mwst / 100);
+		return bruttogesamtpreis;
+	}
+	
+	public static void rechungausgeben(String artikel, int anzahl, double nettogesamtpreis, double bruttogesamtpreis, double mwst) {
+		System.out.println("\tRechnung");
+        System.out.printf("\t\t Netto:  %-20s %6d %10.2f %n", artikel, anzahl, nettogesamtpreis);
+        System.out.printf("\t\t Brutto: %-20s %6d %10.2f (%.1f%s)%n", artikel, anzahl, bruttogesamtpreis, mwst, "%");
+	}
+	
+	
+
+    public static void main(String[] args) {
+    	Scanner myScanner = new Scanner(System.in);
+
+        // Benutzereingaben lesen
+        String artikel = liesString(myScanner);
+        int anzahl = liesInt(myScanner);
+        double preis = liesDoublepreis(myScanner);
+        double mwst = liesDoublemwst(myScanner);
+       
+        // Verarbeiten
+        double nettogesamtpreis = berechneGesamtnettopreis(anzahl,preis);
+        double bruttogesamtpreis = berechneGesamtbruttopreis(nettogesamtpreis, mwst);
+
+        // Ausgeben
+        rechungausgeben(artikel, anzahl, nettogesamtpreis, bruttogesamtpreis, mwst);
+    }
+}
