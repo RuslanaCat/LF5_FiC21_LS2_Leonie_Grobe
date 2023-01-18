1aab8a9364008a18d1c165d063480fc860541c12
RuslanaCat <leoniecat.uwu@gmail.com>
2023-01-18 10:05:47 +0100

    Volumen Datei Verfollständigt

--- LS2_Konfiguration/src/Volumen.java
+++ LS2_Konfiguration/src/Volumen.java
@@ -1,16 +1,22 @@
+import java.util.Scanner;
 
 public class Volumen {
 
-	public static double volumen_rechnung() {
+	public static double volumen_rechnung(Scanner tastatur) {
 		double volumen;
-		
-		
+		System.out.print("bitte geben sie die seite des würfels ein: ");
+		volumen = tastatur.nextDouble();
+		volumen=volumen*volumen*volumen;		
 		return volumen;
 	}
 	
 	public static void main(String[] args) {
-		
-
+		Scanner tastatur = new Scanner(System.in);
+		double Ergebniss;
+		Ergebniss=volumen_rechnung(tastatur);
+		System.out.println("das Volumen Beträgt: "+Ergebniss);
+		Ergebniss=volumen_rechnung(tastatur);
+		System.out.println("das Volumen Beträgt: "+Ergebniss);	
 	}
 
 }
